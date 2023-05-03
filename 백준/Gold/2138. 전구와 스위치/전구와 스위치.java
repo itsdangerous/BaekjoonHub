58b7c13049a8e6335fdd3941f5b918d151de72ec
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2023-05-03 14:09:02 +0900

    [Gold V] Title: 전구와 스위치, Time: 124 ms, Memory: 16748 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+// BJ 2138 : 전구와 스위치
 public class Main {
 	static boolean isSame(int N, int[] cur, int[] target) {
 		for (int i = 0; i < N; i++) {
@@ -13,73 +14,64 @@ public class Main {
 		return true;
 	}
 	
+	static int process(int N, int[] curBlubStatus, int[] targetBlubStatus) {
+		int count = 0;
+		
+		for (int i = 1; i < N - 1; i++) {
+			if (curBlubStatus[i-1] != targetBlubStatus[i-1]) {
+				curBlubStatus[i-1] = (curBlubStatus[i-1] + 1) % 2;
+				curBlubStatus[i] = (curBlubStatus[i] + 1) % 2;
+				curBlubStatus[i+1] = (curBlubStatus[i+1] + 1) % 2;
+				count++;
+			}
+		}
+		if (curBlubStatus[N-2] != targetBlubStatus[N-2]) {
+			curBlubStatus[N-2] = (curBlubStatus[N-2] + 1) % 2;
+			curBlubStatus[N-1] = (curBlubStatus[N-1] + 1) % 2;
+			count++;
+		}
+		
+		return count;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] originStatus = new int[N];
-		int[] targetStatus = new int[N];
+		int[] originBlubStatus = new int[N];
+		int[] targetBlubStatus = new int[N];
 		
-		char[] inputOriginStatus = br.readLine().toCharArray();
-		char[] inputTargetStatus = br.readLine().toCharArray();
+		char[] inputOriginBlubStatus = br.readLine().toCharArray();
+		char[] inputTargetBlubStatus = br.readLine().toCharArray();
 		for (int i = 0; i < N; i++) {
-			originStatus[i] = inputOriginStatus[i] - '0';
-			targetStatus[i] = inputTargetStatus[i] - '0';
+			originBlubStatus[i] = inputOriginBlubStatus[i] - '0';
+			targetBlubStatus[i] = inputTargetBlubStatus[i] - '0';
+		}
+
+		// 첫번째 스위치를 킨 경우
+		int[] curBlubStatus = Arrays.copyOf(originBlubStatus, N);
+		curBlubStatus[0] = (curBlubStatus[0] + 1) % 2;
+		curBlubStatus[1] = (curBlubStatus[1] + 1) % 2;
+		int countTurnOnFirstSwitch = process(N, curBlubStatus, targetBlubStatus) + 1;
+		if (!isSame(N, curBlubStatus, targetBlubStatus)) {
+			countTurnOnFirstSwitch = -1;
+		}
+
+		// 첫번째 스위치를 키지 않은 경우
+		curBlubStatus = Arrays.copyOf(originBlubStatus, N);
+		int countTurnOffFirstSwitch = process(N, curBlubStatus, targetBlubStatus);
+		if (!isSame(N, curBlubStatus, targetBlubStatus)) {
+			countTurnOffFirstSwitch = -1;
 		}
 		
-		int count = 0;
-		int[] curStatus = Arrays.copyOf(originStatus, N);
-		
-		curStatus[0] = (curStatus[0] + 1) % 2;
-		curStatus[1] = (curStatus[1] + 1) % 2;
-		count++;
-		
-		for (int i = 1; i < N - 1; i++) {
-			if (curStatus[i-1] != targetStatus[i-1]) {
-				curStatus[i-1] = (curStatus[i-1] + 1) % 2;
-				curStatus[i] = (curStatus[i] + 1) % 2;
-				curStatus[i+1] = (curStatus[i+1] + 1) % 2;
-				count++;
-			}
-		}
-		if (curStatus[N-2] != targetStatus[N-2]) {
-			curStatus[N-2] = (curStatus[N-2] + 1) % 2;
-			curStatus[N-1] = (curStatus[N-1] + 1) % 2;
-			count++;
-		}
-		if (!isSame(N, curStatus, targetStatus)) {
-			count = -1;
-		}
-		
-		int cnt = 0;
-		curStatus = Arrays.copyOf(originStatus, N);
-		for (int i = 1; i < N - 1; i++) {
-			if (curStatus[i-1] != targetStatus[i-1]) {
-				curStatus[i-1] = (curStatus[i-1] + 1) % 2;
-				curStatus[i] = (curStatus[i] + 1) % 2;
-				curStatus[i+1] = (curStatus[i+1] + 1) % 2;
-				cnt++;
-			}
-		}
-		if (curStatus[N-2] != targetStatus[N-2]) {
-			curStatus[N-2] = (curStatus[N-2] + 1) % 2;
-			curStatus[N-1] = (curStatus[N-1] + 1) % 2;
-			cnt++;
-		}
-		if (!isSame(N, curStatus, targetStatus)) {
-			cnt = -1;
-		}
-		
-		if (count >= 0 && cnt >= 0) {
-			System.out.println(Math.min(count, cnt));
-		} else if (count == -1 && cnt == -1){
+		if (countTurnOnFirstSwitch >= 0 && countTurnOffFirstSwitch >= 0) {
+			System.out.println(Math.min(countTurnOnFirstSwitch, countTurnOffFirstSwitch));
+		} else if (countTurnOnFirstSwitch == -1 && countTurnOffFirstSwitch == -1){
 			System.out.println(-1);
 		} else {
-			System.out.println(Math.max(count, cnt));
+			System.out.println(Math.max(countTurnOnFirstSwitch, countTurnOffFirstSwitch));
 		}
-		
 		
 		br.close();
 	}
-
 }
