8adcf183294e926b859c8f415612998529b2e79a
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2023-10-24 11:15:18 +0900

    [Gold IV] Title: 스도쿠, Time: 1808 ms, Memory: 124268 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,4 +1,17 @@
-def check(num: int, r: int, c: int, arr: list) -> bool:
+import sys
+
+input = sys.stdin.readline
+arr = []
+blank = []
+arr = [list(map(int, input().split())) for _ in range(9)]
+for i in range(9):
+    for j in range(9):
+        if arr[i][j] == 0:
+            blank.append([i, j])
+R = len(blank)
+
+
+def check(num: int, r: int, c: int) -> bool:
     if num in arr[r]:
         return False
 
@@ -15,22 +28,21 @@ def check(num: int, r: int, c: int, arr: list) -> bool:
     return True
 
 
-def solve(arr):
-    for r in range(9):
-        for c in range(9):
-            if arr[r][c] == 0:
-                for i in range(1, 10):
-                    if check(i, r, c, arr):
-                        arr[r][c] = i
-                        if solve(arr):
-                            return True
-                    arr[r][c] = 0
-                return False
-    return True
+def dfs(cnt: int):
+    if cnt == R:
+        return True
+
+    r, c = blank[cnt]
+    for i in range(1, 10):
+        if check(i, r, c):
+            arr[r][c] = i
+            result = dfs(cnt + 1)
+            if result:
+                return True
+            arr[r][c] = 0
+    return False
 
 
-arr = [list(map(int, input().split())) for _ in range(9)]
-solve(arr)
-
-for row in arr:
-    print(" ".join(map(str, row)))
+dfs(0)
+for i in arr:
+    print(*i)
