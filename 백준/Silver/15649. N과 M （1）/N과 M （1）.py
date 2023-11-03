5a52bcecdbc80f7483472872aef57cbbbb1b5684
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2023-11-03 22:38:32 +0900

    [Silver III] Title: N과 M (1), Time: 180 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,31 @@
+import sys
+
+# sys.stdin = open("input.txt", "rt")
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+nums = [i + 1 for i in range(N)]
+
+select_nums = [0 for _ in range(M)]
+
+visited = [False for _ in range(N)]
+
+
+def permutation(cnt: int):
+    if cnt == M:
+        print(*select_nums)
+        return
+
+    for i in range(N):
+        if visited[i]:
+            continue
+
+        visited[i] = True
+        select_nums[cnt] = nums[i]
+        permutation(cnt + 1)
+        select_nums[cnt] = 0
+        visited[i] = False
+
+
+permutation(0)
