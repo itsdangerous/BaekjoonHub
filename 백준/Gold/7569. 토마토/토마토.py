c3de36dfae26f0de13ca66ceeb00e1dc32f69576
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2025-09-24 00:58:34 +0900

    [Gold V] Title: 토마토, Time: 4784 ms, Memory: 62544 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,73 +1,54 @@
 from collections import deque
 
+direction = [
+    [-1, 0, 0],
+    [1, 0, 0],
+    [0, 0, 1],
+    [0, 0, -1],
+    [0, 1, 0],
+    [0, -1, 0],
+]
 
-def print_arr_3(arr: list) -> None:
-    print(f"time = {time} ----------------")
-
-    for i in arr:
-        for j in i:
-            for k in j:
-                print(k, end=" ")
-            print()
-        print()
-    print(f"time = {time} ----------------")
-
-
-def check_out_of_index(h: int, r: int, c: int) -> bool:
-    return 0 <= h < H and 0 <= r < N and 0 <= c < M
-
-
-direction = [[1, 0, 0], [-1, 0, 0], [0, -1, 0], [0, 0, 1], [0, 1, 0], [0, 0, -1]]
 M, N, H = map(int, input().split())
-visited = [[[False for _ in range(M)] for _ in range(N)] for _ in range(H)]
-not_success = 0
-time = -1
+
+def searchable(z, r, c):
+    return 0 <= z < H and 0 <= r < N and 0 <= c < M
+
+arr = []
 que = deque()
-
-arr = [[[int(x) for x in input().split()] for _ in range(N)] for _ in range(H)]
-
+visited = [[[False]*M for i in range(N)] for h in range(H)]
+time = -1
+targets = 0
 for h in range(H):
-    for r in range(N):
-        for c in range(M):
-            if arr[h][r][c] == 1:
-                que.append([h, r, c])
-            if arr[h][r][c] == 0:
-                not_success += 1
-if not_success == 0:
-    print(0)
+    arr.append([])
+    for i in range(N):
+        line = list(map(int, input().split()))
+        arr[h].append(line)
+        for j in range(M):
+            if arr[h][i][j] == 1:
+                que.appendleft([h,i,j])
+                visited[h][i][j] = True
+            elif arr[h][i][j] == 0:
+                targets += 1
 
+while que:
+    time += 1
+    len_que = len(que)
+    while len_que > 0:
+        len_que -= 1
+        cur_z, cur_r, cur_c = que.pop()
+        for dz, dr, dc in direction:
+            nz, nr, nc = cur_z + dz, cur_r + dr, cur_c + dc
+            if not searchable(nz, nr, nc): continue
+            if visited[nz][nr][nc]: continue
+            if arr[nz][nr][nc] == -1: continue
+            if arr[nz][nr][nc] == 1: continue
+
+            visited[nz][nr][nc] = True
+            arr[nz][nr][nc] = 1
+            que.appendleft([nz, nr, nc])
+            targets -= 1
+if targets:
+    print(-1)
 else:
-    while que:
-        len_que = len(que)
-        time += 1
-        # print_arr_3(arr)
-
-        while len_que > 0:
-            pos = que.popleft()
-            h, r, c = pos[0], pos[1], pos[2]
-            visited[h][r][c] = True
-            for dh, dr, dc in direction:
-                # print(f"dh, dr, dc = {dh}, {dr}, {dc}")
-                nh = h + dh
-                nr = r + dr
-                nc = c + dc
-                # print(f"nh, nr, nc= {nh}, {nr}, {nc}")
-
-                if not check_out_of_index(nh, nr, nc):
-                    continue
-                if visited[nh][nr][nc]:
-                    continue
-                if arr[nh][nr][nc] == -1:
-                    visited[nh][nr][nc] = True
-                    continue
-                if arr[nh][nr][nc] == 1:
-                    continue
-                que.append([nh, nr, nc])
-                visited[nh][nr][nc] = True
-                arr[nh][nr][nc] = 1
-                not_success -= 1
-            len_que -= 1
-    if not_success > 0:
-        print(-1)
-    else:
-        print(time)
+    print(time)
