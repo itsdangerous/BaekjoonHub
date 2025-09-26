b05e0eb381f9ba3419023f33d35c43509908442a
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2025-09-26 23:50:45 +0900

    [Gold I] Title: 낚시왕, Time: 820 ms, Memory: 35480 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -1,5 +1,3 @@
-import sys, copy
-# sys.stdin = open("input.txt", "r")
 R, C, M = map(int, input().split())
 
 directions = [
@@ -14,31 +12,30 @@ def inline(r1, c1):
     global R, C
     return 0 <= r1 < R-1 and 0 <= c1 < C-1
 
-def get_next_loc(i, j, speed, dir):
-
-    if dir == 0 or dir == 2:  # i
-        cycle = R * 2 - 2
-        if dir == 0:
-            speed += 2 * (R - 1) - i
-        else:
-            speed += i
-
-        speed %= cycle
-        if speed >= R:
-            return (2 * R - 2 - speed, j, 0)
-        return (speed, j, 2)
-
-    else:  # j
-        cycle = C * 2 - 2
-        if dir == 3:
-            speed += 2 * (C - 1) - j
-        else:
-            speed += j
-
-        speed %= cycle
+def get_next_loc(r, c, speed, d):
+    # 좌 우
+    if d in (1, 3):
+        cycle = 2 * C - 2
+        if d == 1: # 우
+            speed += c
+        else: # 좌
+            speed += 2*C-2 - c
+        speed = speed % cycle
         if speed >= C:
-            return (i, 2 * C - 2 - speed, 3)
-        return (i, speed, 1)
+            return r, 2*C-2-speed, 3
+        return r, speed, 1
+
+    # 상 하
+    elif d in (0, 2):
+        cycle = 2*R - 2
+        if d == 2: # 하
+            speed += r
+        else:
+            speed += 2*R-2 - r
+        speed = speed % cycle
+        if speed >= R:
+            return 2*R-2-speed, c, 0
+        return speed, c, 2
 
 class Shark:
     global arr
@@ -50,18 +47,10 @@ class Shark:
         self.speed = speed
         self.direction = direction
         self.size = size
-        self.is_alive = True
 
     def move(self):
-        if not self.is_alive: return
-        time = self.speed
         origin_r, origin_c = self.r, self.c
-        # while time > 0:
-        #     time -= 1
-        #     nr, nc = self.r + directions[self.direction][0], self.c + directions[self.direction][1]
-        #     if not inline(nr, nc):
-        #         self.direction = (self.direction + 2) %  4
-        #         nr, nc = self.r + directions[self.direction][0], self.c + directions[self.direction][1]
+
         nr, nc, nd = get_next_loc(origin_r, origin_c, self.speed, self.direction)
         self.r, self.c, self.direction = nr, nc, nd
         # 원래 있던 자리 -1로
@@ -69,18 +58,10 @@ class Shark:
 
         # 이동한 shark의 위치는, 모든 shark가 이동한 후 한번에 arr에 찍어준다.
 
-def print_list(l: list):
-    for a in range(len(l)):
-        for b in range(len(l[a])):
-            print(l[a][b], end=' ')
-        print()
-
 arr = [[-1] * C for _ in range(R)]  # 가장 큰 물고기의 id가 존재하는 바다 배열
 
-# sharks = []
 sharks = {}
 for case in range(M):
-    # arr.append(list(map(int, input().split())))
     r, c, s, d, z = map(int, input().split())
 
     if d == 1:
@@ -92,18 +73,12 @@ for case in range(M):
     elif d == 4:
         d = 3
     shark = Shark(case, r-1, c-1, s, d, z)
-    # sharks.append(shark)
     sharks[case] = shark
     arr[r-1][c-1] = shark.id
-    # r,c : 좌표
-    # s: 속력
-    # d: 이동 방향
-    # z: 사이즈
 
 def delete_shark(shark_id):
     global arr, sharks
     sk = sharks[shark_id]
-    sk.is_alive = False
     arr[sk.r][sk.c] = -1
     del sharks[shark_id]
 
@@ -111,8 +86,8 @@ result = 0
 def simulation():
     global arr, result, sharks
     # 1. person move
-        # 2. catch (collect sharks)
     for man_c in range(C):
+        # 2. catch (collect sharks)
         found_shark_id = None
         for r in range(R):
             if arr[r][man_c] != -1:
@@ -120,35 +95,28 @@ def simulation():
                 break
         if found_shark_id is not None:
             found_shark = sharks[found_shark_id]
-            if found_shark.is_alive:
-                result += found_shark.size
-                found_shark.is_alive = False
-                delete_shark(found_shark_id)
-                arr[found_shark.r][found_shark.c] = -1
+            result += found_shark.size
+            delete_shark(found_shark_id)
+            arr[found_shark.r][found_shark.c] = -1
 
         for k, v in sharks.items():
             v.move()
 
-        # 4. 모든 샤크는 이동했으며, 같은 위치에 있는 경우 잡아먹는다.
+        # 3. 모든 샤크는 이동했으며, 같은 위치에 있는 경우 잡아먹는다.
         for k, sk in list(sharks.items()):
-            if not sk.is_alive: continue
             prev_shark_id = arr[sk.r][sk.c]
             if prev_shark_id != -1: # 이미 해당 포지션에 shark id가 존재한다면,
                 prev_shark = sharks[prev_shark_id]
                 # 큰놈이 작은놈 잡아먹음
                 # 크기가 같은 경우는 없음
                 if sk.size > prev_shark.size:
-                    prev_shark.is_alive = False
                     del sharks[prev_shark_id]
                     arr[sk.r][sk.c] = sk.id
                 else:
-                    sk.is_alive = False
                     del sharks[sk.id]
                     arr[prev_shark.r][prev_shark.c] = prev_shark.id
             else: arr[sk.r][sk.c] = sk.id
         sharks = sharks
-        # print_list(arr)
-        # print()
 
 simulation()
 print(result)
