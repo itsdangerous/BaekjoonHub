5d9ce81b8927b32443e8e1f43997718065261204
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2026-02-23 23:30:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,19 +1,10 @@
-def reverse_arr(i: int, j: int, arr: list) -> None:
-    index_left = i
-    index_right = j
-    while index_left <= (i + j) // 2:
-        arr[index_left], arr[index_right] = arr[index_right], arr[index_left]
-        index_left += 1
-        index_right -= 1
-
-
 N, M = map(int, input().split())
 
-arr = [i for i in range(N + 1)]
+arr = [i+1 for i in range(N)]
 
 for i in range(M):
     a, b = map(int, input().split())
-    reverse_arr(a, b, arr)
-
-for i in range(1, N + 1):
-    print(arr[i], end=" ")
+    split_arrs = [arr[:a-1], arr[a-1:b], arr[b:]]
+    split_arrs[1] = split_arrs[1][::-1]
+    arr = split_arrs[0] + split_arrs[1] + split_arrs[2]
+print(*arr)
