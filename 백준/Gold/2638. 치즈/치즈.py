3f7923bd164c287a0a2fb7323dbda5e026b643c0
SeunggyuLEE <76903093+itsdangerous@users.noreply.github.com>
2026-03-05 21:11:12 +0900

    [Gold III] Title: 치즈, Time: 756 ms, Memory: 35016 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,89 +1,87 @@
 from collections import deque
 
 
-def check(r: int, c: int) -> bool:
+def check(r, c):
     return 0 <= r < N and 0 <= c < M
 
 
-def count_cheeses(map: list) -> int:
-    count = 0
-    for i in map:
-        for j in i:
-            if j == 1:
-                count += 1
-    return count
-
-
-# 공기는 9로 마킹하기
-def mark(map: list) -> None:
-    visited = [[False for _ in range(M)] for _ in range(N)]
-    que = deque()
-    que.append([0, 0])
-    map[0][0] = 9
-    visited[0][0] = True
-
-    while que:
-        pos = que.popleft()
-        for i in range(4):
-            nr = pos[0] + dr[i]
-            nc = pos[1] + dc[i]
-            if not check(nr, nc):
-                continue
-            if visited[nr][nc]:
-                continue
-            if map[nr][nc] == 1:
-                continue
-            que.append([nr, nc])
-            map[nr][nc] = 9
-            visited[nr][nc] = True
-
-
-def melt(map: list, cnt_cheese: int) -> int:
-    melted_cheeses = deque()
-
-    for r in range(N):
-        for c in range(M):
-            if map[r][c] == 1:
-                cnt_air = 0
-                for k in range(4):
-                    nr = r + dr[k]
-                    nc = c + dc[k]
-                    if not check(nr, nc):
-                        continue
-                    if map[nr][nc] != 9:
-                        continue
-                    cnt_air += 1
-                    if cnt_air == 2:
-                        break
-                if cnt_air != 2:
-                    continue
-                cnt_cheese -= 1
-                map[r][c] = 2
-                melted_cheeses.append([r, c])
-
-    while melted_cheeses:
-        melt_pos = melted_cheeses.popleft()
-        map[melt_pos[0]][melt_pos[1]] = 9
-    return cnt_cheese
-
+directions = [
+    [-1, 0],
+    [0, 1],
+    [1, 0],
+    [0, -1],
+]
 
 N, M = map(int, input().split())
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
 
-map = [list(map(int, input().split())) for _ in range(N)]
-cnt_cheese = count_cheeses(map)
+arr = []
+sum_cheese = 0
+for i in range(N):
+    line = list(map(int, input().split()))
+    arr.append(line)
+    sum_cheese += sum(line)
 
-time = 0
-while cnt_cheese > 0:
-    mark(map)
-    # print(cnt_cheese)
-    # for i in map:
+
+def mark_air_to_nine():
+    que = deque()
+    visited = [[False] * M for _ in range(N)]
+    que.append([0, 0])
+    visited[0][0] = True
+    arr[0][0] = 9
+    while que:
+        cr, cc = que.popleft()
+        for dr, dc in directions:
+            nr, nc = cr + dr, cc + dc
+            if not check(nr, nc): continue
+            if visited[nr][nc]: continue
+            if arr[nr][nc] != 0 and arr[nr][nc] != 9: continue
+            arr[nr][nc] = 9
+            visited[nr][nc] = True
+            que.append([nr, nc])
+
+
+def remove_cheese():
+    removed_cheese = []
+    cnt_removed_cheese = 0
+    for r in range(N):
+        for c in range(M):
+            if arr[r][c] != 1: continue
+            sum_air = 0
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if not check(nr, nc): continue
+                if arr[nr][nc] == 9: sum_air += 1
+                if sum_air >= 2: break
+
+            if sum_air >= 2:
+                removed_cheese.append([r, c])
+                cnt_removed_cheese += 1
+
+    for r, c in removed_cheese:
+        arr[r][c] = 9
+
+    return cnt_removed_cheese, removed_cheese
+
+
+
+cnt = 0
+while sum_cheese > 0:
+    # for i in arr:
     #     print(i)
-    # print("---------")
-    time += 1
-    cnt_cheese = melt(map, cnt_cheese)
-# for i in map:
-#     print(i)
-# print("---------")
-print(time)
+    # print()
+    # 외부 공기를 9로 마킹
+    mark_air_to_nine()
+    # for i in arr:
+    #     print(i)
+    # print()
+    # 2개 이상 외부 공기(9)가 인접한 치즈 찾아서 제거 및 제거된 치즈 개수만큼 전체 치즈 개수 업데이트
+    cnt_removed_cheese, removed_cheese = remove_cheese()
+    # print(removed_cheese)
+    sum_cheese -= cnt_removed_cheese
+    # for i in arr:
+    #     print(i)
+    # print()
+    # 시간 증가
+    cnt += 1
+
+print(cnt)
